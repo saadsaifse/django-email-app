28da76c5f50059d4bcb08159cb9a856ddf4e2e1c
Saad Saif <msaadsaif5@gmail.com>
2023-10-11 20:23:09 -0400

    add rest framework and user management

--- app/emailSender/models.py
+++ app/emailSender/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+# class Email(models.Model):
+#     """Recipe object."""
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#     )
+#     title = models.CharField(max_length=255)
+#     description = models.TextField(blank=True)
+#     time_minutes = models.IntegerField()
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     link = models.CharField(max_length=255, blank=True)
+#     tags = models.ManyToManyField('Tag')
+#     ingredients = models.ManyToManyField('Ingredient')
+#     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+#     def __str__(self):
+#         return self.title
